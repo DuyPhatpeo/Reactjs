8e043317f19a67156c043b8ebe8d6dae0c6e77ec
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-08 09:46:38 +0700

    Ẩn hiện JSX và Component trong câu điều kiện if else

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,45 +1,14 @@
-import Courses from "./components/Course";
-import MyFooter from "./components/Footer";
-import MyHeader from "./components/Header";
-import Product from "./components/Product";
+import React from "react";
+import Category from "./components/Category";
+import NotCategory from "./components/NotCategory";
 
 function App() {
-  const listProducts = [
-    {
-      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
-      title: "SHOPEE PREMIUM",
-      price: 1000,
-    },
-    {
-      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
-      title: "THƯƠNG HIỆU QUỐC TẾ",
-      price: 1200,
-    },
-    {
-      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
-      title: "SHOPEE BOOK CLUB",
-      price: 1300,
-    },
-    {
-      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
-      title: "BRAND MEMBERSHIP",
-      price: 1400,
-    },
-  ];
-  return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      {listProducts.map((product, index) => {
-        return (
-          <Product
-            title={product.title}
-            url={product.url}
-            price={product.price}
-            key={index}
-          />
-        );
-      })}
-    </div>
-  );
+  const isRenderCategory = false;
+  if (isRenderCategory) {
+    return <Category />;
+  } else {
+    return <NotCategory />;
+  }
 }
 
 export default App;
