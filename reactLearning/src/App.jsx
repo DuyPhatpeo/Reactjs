56130896a07083063818a09013fad57858de5412
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-08 09:53:00 +0700

    Render JSX thông qua variable

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Category from "./components/Category";
-import NotCategory from "./components/NotCategory";
 
 function App() {
-  const isRenderCategory = false;
-  if (isRenderCategory) {
-    return <Category />;
-  } else {
-    return <NotCategory />;
+  const content1 = <div>This is content 1</div>;
+  const content2 = <div>This is content 2</div>;
+
+  const isDisplay1 = true;
+  if (isDisplay1) {
+    return content1;
   }
+  return content2;
 }
 
 export default App;
