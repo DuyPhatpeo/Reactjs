adde3b91370b84b0c4d812f12e730cefdffa934f
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-08 09:35:21 +0700

    Render 1 Component bên trong vòng lặp

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,12 +1,44 @@
 import Courses from "./components/Course";
 import MyFooter from "./components/Footer";
 import MyHeader from "./components/Header";
+import Product from "./components/Product";
 
 function App() {
+  const listProducts = [
+    {
+      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
+      title: "SHOPEE PREMIUM",
+      price: 1000,
+    },
+    {
+      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
+      title: "THƯƠNG HIỆU QUỐC TẾ",
+      price: 1200,
+    },
+    {
+      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
+      title: "SHOPEE BOOK CLUB",
+      price: 1300,
+    },
+    {
+      url: "https://cdn11.dienmaycholon.vn/filewebdmclnew/public/userupload/files/Image%20FP_2024/avatar-cute-3.jpg",
+      title: "BRAND MEMBERSHIP",
+      price: 1400,
+    },
+  ];
   return (
-    <>
-      <Courses />
-    </>
+    <div style={{ display: "flex", gap: "20px" }}>
+      {listProducts.map((product, index) => {
+        return (
+          <Product
+            title={product.title}
+            url={product.url}
+            price={product.price}
+            key={index}
+          />
+        );
+      })}
+    </div>
   );
 }
 
