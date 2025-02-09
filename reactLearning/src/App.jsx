7adbf23620c2409a1ea772e56e1419b3992044fb
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-09 09:59:53 +0700

    One-way binding

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(() => {
-    const number = 1 + 1 + 1;
-    console.log(number);
-    return number;
-  });
-
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
+  const [userName, setUsername] = useState();
+  const handleGetUsername = (e) => {
+    setUsername(e.target.value);
   };
-
+  console.log(userName);
   return (
     <>
-      <div>Sử dụng useState trong reactJS</div>
-      <div>Count: {count}</div>
-      <button onClick={handleClick}>Click me</button>
+      <div>
+        <input type="text" onChange={(e) => handleGetUsername(e)} />
+      </div>
     </>
   );
 }
