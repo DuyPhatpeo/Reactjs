75ec2f8ecdadd27a9d7f70b19d77738971227284
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-09 10:28:16 +0700

    Two-Way Binding | Dữ liệu 2 chiều

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,19 +1,59 @@
 import React, { useState } from "react";
 
 function App() {
-  const [userName, setUsername] = useState();
-  const handleGetUsername = (e) => {
-    setUsername(e.target.value);
+  // const colors = ["#FF0000", "#0000FF", "#FF00FF"];
+  // const [color, setColor] = useState("#FF0000");
+
+  // const handleChangeColor = (index) => {
+  //   console.log(index);
+  //   setColor(colors[index]);
+  // };
+  const [chosseProduct, setChosseProduct] = useState(1);
+
+  const products = [
+    { id: 1, name: "IPhone 13" },
+    { id: 2, name: "IPhone 14" },
+    { id: 3, name: "IPhone 15" },
+  ];
+
+  const handleGetValueCheck = (id) => {
+    setChosseProduct(id);
   };
-  console.log(userName);
+  console.log(chosseProduct);
+
   return (
     <>
+      {/* <div
+        style={{
+          background: color,
+          width: "70px",
+          height: "30px",
+          marginBottom: "10px",
+        }}
+      ></div>
+
       <div>
-        <div>Username: {userName}</div>
-        <input type="text" onChange={(e) => handleGetUsername(e)} />
+        <button onClick={() => handleChangeColor(0)}>Red</button>
+        <button onClick={() => handleChangeColor(1)}>Blue</button>
+        <button onClick={() => handleChangeColor(2)}>Pink</button>
+      </div> */}
+
+      <div>
+        {products.map((product, index) => {
+          return (
+            <div key={index}>
+              <label>{product.name}</label>
+              <input
+                type="radio"
+                value={product.id}
+                onChange={(e) => handleGetValueCheck(e.target.value)}
+                checked={+chosseProduct === product.id}
+              />
+            </div>
+          );
+        })}
       </div>
     </>
   );
 }
-
 export default App;
