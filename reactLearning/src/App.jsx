fb81eb7b8499b156cff57205f80838055fde53de
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-11 09:59:04 +0700

    Thuật ngữ Mount - Unmount

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,40 +1,18 @@
 import React, { useState } from "react";
+import Content from "./components/Content";
 
 function App() {
-  const [chooseProduct, setChooseProduct] = useState([]);
+  const [show, setShow] = useState(false);
 
-  const products = [
-    { id: 1, name: "IPhone 13" },
-    { id: 2, name: "IPhone 14" },
-    { id: 3, name: "IPhone 15" },
-  ];
-
-  const handleGetValueCheck = (id) => {
-    if (!chooseProduct.includes(+id)) {
-      setChooseProduct((prev) => [...prev, +id]);
-    } else {
-      setChooseProduct((prev) => prev.filter((idProduct) => idProduct !== +id));
-    }
+  const handleShow = () => {
+    setShow(!show);
   };
-
-  console.log(chooseProduct);
 
   return (
     <>
       <div>
-        {products.map((product, index) => {
-          return (
-            <div key={index}>
-              <label>{product.name}</label>
-              <input
-                type="checkbox"
-                value={product.id}
-                onChange={(e) => handleGetValueCheck(e.target.value)}
-                checked={chooseProduct.includes(product.id)}
-              />
-            </div>
-          );
-        })}
+        <button onClick={handleShow}>Show</button>
+        {show && <Content />}
       </div>
     </>
   );
