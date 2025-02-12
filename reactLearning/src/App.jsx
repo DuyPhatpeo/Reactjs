2b67f7613d6466fb6619d1b04f34e0b7d7c7ad72
Tran Duy Phat <phattranduy00@gmail.com>
2025-02-12 21:18:51 +0700

    Tạo ra Todo-List App sử dụng useState()

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
-import Content from "./components/Content";
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [todo, setTodo] = useState({ id: "", name: "" });
+  const [todoList, setTodoList] = useState([
+    { id: 1, name: "Đánh răng rửa mặt" },
+  ]);
 
-  const handleShow = () => {
-    setShow(!show);
+  const handleGetToto = (value) => {
+    setTodo((prev) => ({
+      ...prev,
+      id: prev.id || Math.floor(Math.random() * 100000), // Chỉ tạo id khi cần
+      name: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (!todo.name.trim()) return;
+
+    setTodoList((prev) => [...prev, todo]);
+    setTodo({ id: "", name: "" }); // Reset input
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodoList((prev) => prev.filter((todo) => todo.id !== id));
   };
 
   return (
-    <>
+    <div>
       <div>
-        <button onClick={handleShow}>Show</button>
-        {show && <Content />}
+        <input
+          type="text"
+          onChange={(e) => handleGetToto(e.target.value)}
+          value={todo.name}
+        />
+        <button onClick={handleSubmit}>Add</button>
       </div>
-    </>
+
+      <ul>
+        {todoList.map((todo) => (
+          <div style={{ display: "flex", gap: "30px" }} key={todo.id}>
+            <li>{todo.name}</li>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              x
+            </div>
+          </div>
+        ))}
+      </ul>
+    </div>
   );
 }
+
 export default App;
