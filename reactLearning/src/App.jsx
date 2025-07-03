984a60041f86dc23f028f7bc15dc3d3606dee71a
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-03 17:44:39 +0700

    useEffect() p1

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -1,54 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { use } from "react";
 
 function App() {
-  const [todo, setTodo] = useState({ id: "", name: "" });
-  const [todoList, setTodoList] = useState([
-    { id: 1, name: "Đánh răng rửa mặt" },
-  ]);
+  const [count, setCount] = useState(1);
 
-  const handleGetToto = (value) => {
-    setTodo((prev) => ({
-      ...prev,
-      id: prev.id || Math.floor(Math.random() * 100000), // Chỉ tạo id khi cần
-      name: value,
-    }));
-  };
+  const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   // fetch("https://jsonplaceholder.typicode.com/posts")
+  //   //   .then((response) => response.json())
+  //   //   .then((json) => {
+  //   //     setPosts(json);
+  //   //   });
+  // });
 
-  const handleSubmit = () => {
-    if (!todo.name.trim()) return;
+  // useEffect callback, []) nó chỉ chạy 1 lần khi component mount
 
-    setTodoList((prev) => [...prev, todo]);
-    setTodo({ id: "", name: "" }); // Reset input
-  };
-
-  const handleDeleteTodo = (id) => {
-    setTodoList((prev) => prev.filter((todo) => todo.id !== id));
-  };
-
+  useEffect(() => {
+    console.log("re-render");
+  }, []);
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => handleGetToto(e.target.value)}
-          value={todo.name}
-        />
-        <button onClick={handleSubmit}>Add</button>
-      </div>
+      <h1>Counter: {count}</h1>
+      {console.log("render JSX")}
+      <button onClick={() => setCount(count + 1)}>Click me</button>
 
-      <ul>
-        {todoList.map((todo) => (
-          <div style={{ display: "flex", gap: "30px" }} key={todo.id}>
-            <li>{todo.name}</li>
-            <div
-              style={{ cursor: "pointer" }}
-              onClick={() => handleDeleteTodo(todo.id)}
-            >
-              x
-            </div>
-          </div>
-        ))}
-      </ul>
+      {posts.map((post, index) => (
+        <div key={index}>{post.title}</div>
+      ))}
     </div>
   );
 }
