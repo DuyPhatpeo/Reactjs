75b503e6b61eda8a267e60e16f671d8dfdc39c9a
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-03 17:55:37 +0700

    useEffect() P2

--- reactLearning/src/App.jsx
+++ reactLearning/src/App.jsx
@@ -3,25 +3,42 @@ import { use } from "react";
 
 function App() {
   const [count, setCount] = useState(1);
+  const [color, setColor] = useState("");
 
   const [posts, setPosts] = useState([]);
   // useEffect(() => {
-  //   // fetch("https://jsonplaceholder.typicode.com/posts")
-  //   //   .then((response) => response.json())
-  //   //   .then((json) => {
-  //   //     setPosts(json);
-  //   //   });
-  // });
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setPosts(json);
+  //     });
+  // }, []); // Chạy 1 lần khi component mount
 
   // useEffect callback, []) nó chỉ chạy 1 lần khi component mount
 
+  // useEffect(() => {
+  //   console.log("re-render");
+  // }, []);
+
   useEffect(() => {
-    console.log("re-render");
-  }, []);
+    if (count % 2 === 0) {
+      setColor("red");
+    } else {
+      setColor("black");
+    }
+  }, [count]);
+
   return (
     <div>
       <h1>Counter: {count}</h1>
-      {console.log("render JSX")}
+      <div
+        style={{
+          background: color,
+          width: "100px",
+          height: "100px",
+        }}
+      ></div>
+      {/* {console.log("render JSX")} */}
       <button onClick={() => setCount(count + 1)}>Click me</button>
 
       {posts.map((post, index) => (
